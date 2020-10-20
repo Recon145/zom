3df046d28e3253160d9de4d46a9d3b3c3bf576ad
Recon145 <reconmovyie@gmail.com>
2020-10-20 13:22:35 +0300

    asda

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    socket.emit('chat message', "http://example.com");
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
